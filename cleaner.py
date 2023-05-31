1c73b86c08aeac29ce2254b8620c9b63e4e03b2c
ArktikHunter <81192324+ArktikHunter@users.noreply.github.com>
2023-05-31 11:07:49 -0600

    Cleaner can clip verb form data

--- cleaner.py
+++ cleaner.py
@@ -4,6 +4,33 @@
 Cleans the data
 """
 
-#TODO
+import csv
+
+# Read in the data
+with open("rawdata.txt") as f:
+    data = [ line.strip() for line in f.readlines()]
+
+#DEBUG - display raw data
+print("____RAW_____")
+for line in data[:10]:
+    print(line)         
+
+# Clip the verb form data
+intermediate = []
+for line in data:
+    if '\t' in line:
+        line = line[line.find('\t')+1:]   
+    intermediate.append(line)
+
+#DEBUG - display data after clipping
+print("____INTER_____")
+for line in intermediate[:10]:
+    print(line)         
 
 
+#TODO: decide on english formatting
+
+
+
+# final formatting
+fields = ["Southwestern Ojibwe", "English(raw)", "English(formatted)" ]
